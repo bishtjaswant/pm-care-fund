4c27463970e6bff0ada5709a0ba0f738e0bd8d0d
bishtjaswant <jaswantbishtuk1995@gmail.com>
2020-04-25 20:05:32 +0530

    add alert notification

---
+++ inc/header.php
@@ -0,0 +1,25 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="./assets/css/materia-bootstrap.min.css">
+  <link rel="stylesheet" href="./assets/css/index.css">
+  <title>Prime Minister's National Relief Fund</title>
+
+  <link href="https://pmnrf.gov.in/assets/img/favicon.ico" rel="icon" type="image/ico" />
+  <link rel="stylesheet" href="https://parsleyjs.org/src/parsley.css">
+  <!-- jquer -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- javascript validation -->
+  <script src="https://parsleyjs.org/dist/parsley.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@9.10.12/dist/sweetalert2.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9.10.12/dist/sweetalert2.min.js"></script>
+
+</head>
